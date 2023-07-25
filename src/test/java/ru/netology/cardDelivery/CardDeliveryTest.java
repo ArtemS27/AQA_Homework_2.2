a3cd8ff765cc8e5a53359037379e9b3c0fe322c9
Artem <s-art7@bk.ru>
2023-07-25 20:36:26 +0300

    Add test task 2

--- src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
     @Test
@@ -17,6 +15,23 @@ public class CardDeliveryTest {
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Москва");
         form.$("[data-test-id=date] input").setValue("15.08.2023");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        form.$(".spin_visible").shouldBe(Condition.visible  );
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(13));
+    }
+
+    @Test
+    public void choseCityAndDateFromPopUpMenu(){
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Мо");
+        $$(".popup_visible .menu-item").find(Condition.exactText("Москва")).click();
+        form.$("[data-test-id=date] .icon-button").click();
+        $$(".popup_visible .calendar__arrow_direction_right").last().click();
+        $$(".popup_visible .calendar__day").find(Condition.exactText("4")).click();
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+71234567890");
         form.$("[data-test-id=agreement]").click();
